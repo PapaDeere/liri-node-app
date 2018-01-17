090d821789f1ffbb39c91a78b14184343611dbc9
Papa Deere <papadeere5150@gmail.com>
2018-01-17 16:48:49 -0700

    updated
    added content

--- spotify.js
+++ spotify.js
@@ -1,26 +1,36 @@
-
- var Spotify = require('node-spotify-api');
- var keys = require("./keys.js");
+var Spotify = require('node-spotify-api');
+var keys = require("./keys.js");
 
 var spotify = new Spotify(keys.spotify);
+var songName = "The Sign";
+if (myInput != undefined) {
+    songName = myInput;
+}
+spotify.search({ type: 'track', query: songName, limit: 1 }, function(err, data) {
+    if (err) {
+        return console.log('Error occurred: ' + err);
+    }
 
-spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
-  if (err) {
-    return console.log('Error occurred: ' + err);
-  }
- 
-console.log(data); 
+    console.log("Song Name: " + data.tracks.items[0].name);
+    console.log("Artist: " + data.tracks.items[0].artists[0].name);
+    if (data.tracks.items[0].preview_url !== null) {
+        console.log("Preview: " + data.tracks.items[0].preview_url);
+    }
+    console.log('From the album "' + data.tracks.items[0].album.name + '"');
+
 });
-    // Do something with 'data' 
+},
+};
+// Do something with 'data' 
 // Search tracks whose name, album or artist contains '()'
 //function getSong(){
 //spotify.searchTracks("The Sign by Ace of Base")
-  //.then(function(data) {
-  //  console.log('Search by "The Sign by Ace of Base"', data.body);
-  //}, function(err) {
-  // console.error(err);
-  //});
-  //}
+//.then(function(data) {
+//  console.log('Search by "The Sign by Ace of Base"', data.body);
+//}, function(err) {
+// console.error(err);
+//});
+//}
 //var SpotifyWebApi = require("../");
 
 /*
@@ -33,26 +43,25 @@ console.log(data);
 //var spotifyApi = new SpotifyWebApi();
 
 //spotifyApi.searchTracks('Love', function(err, data) {
- // if (err) {
-  //  console.error('Something went wrong', err.message);
-  //  return;
-  //}
+// if (err) {
+//  console.error('Something went wrong', err.message);
+//  return;
+//}
 
-  // Print some information about the results
-  //console.log('I got ' + data.body.tracks.total + ' results!');
+// Print some information about the results
+//console.log('I got ' + data.body.tracks.total + ' results!');
 
-  // Go through the first page of results
-  //var firstPage = data.body.tracks.items;
-  //console.log('The tracks in the first page are.. (popularity in parentheses)');
+// Go through the first page of results
+//var firstPage = data.body.tracks.items;
+//console.log('The tracks in the first page are.. (popularity in parentheses)');
 
-  /*
-   * 0: All of Me (97)
-   * 1: My Love (91)
-   * 2: I Love This Life (78)
-   * ...
-   */
-  //firstPage.forEach(function(track, index) {
-   // console.log(index + ': ' + track.name + ' (' + track.popularity + ')');
-  //});
+/*
+ * 0: All of Me (97)
+ * 1: My Love (91)
+ * 2: I Love This Life (78)
+ * ...
+ */
+//firstPage.forEach(function(track, index) {
+// console.log(index + ': ' + track.name + ' (' + track.popularity + ')');
 //});
-
+//});
